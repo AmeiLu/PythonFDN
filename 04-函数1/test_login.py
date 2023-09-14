47a4226df51b066f079f9100537e566ed037b5ee
留着空白 <lunanmei@foxmail.com>
2023-09-14 22:44:47 +0800

    feat: file

--- 04-函数1/test_login.py
+++ 04-函数1/test_login.py
@@ -4,4 +4,4 @@ from service.login_service import login
 login('admin', '123456', '6666')
 login('andmin', '123456', '6666')
 login('admin', '1234567', '6666')
-login('admin', '123456', '666633')
+login('admin', '123456', '66666')
